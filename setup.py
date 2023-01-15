1fcb15e8f6b29b0e9968c25f2cb2f8f2640d7796
Atilla Gündoğan <career@at1lla.com>
2023-01-15 05:13:44 +0300

    .

---
+++ setup.py
@@ -0,0 +1,23 @@
+#import nltk
+#import torch
+
+
+#nltk.download('punkt')
+
+
+#pip install torch
+
+#pip install -U setuptools
+
+#python3 -m venv venv
+#virtualenv env
+
+#. .\venv\Scripts\activate
+
+#pip install numpy
+
+#pip install opencv-contrib-python
+#pip install opencv-python
+
+
+
