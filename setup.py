f4782ee7b6aebda6d91e4db7d3bbafba7d8b1b9a
Atilla Gündoğan <career@at1lla.com>
2023-01-20 08:32:44 +0300

    up

--- setup.py
+++ setup.py
@@ -5,20 +5,53 @@ import nltk
 #nltk.download('punkt')
 
 
-#pip install torch
+#pip3 install torch
 
-#pip install -U setuptools
+#pip3 install -U setuptools
 
 #python3 -m venv venv
 #virtualenv env
 
 #. .\venv\Scripts\activate
 
-#pip install numpy
+#pip3 install numpy
 
-#pip install opencv-contrib-python
-#pip install opencv-python
+#pip3 install opencv-contrib-python
+#pip3 install opencv-python
+
+
 
 #pip3 install nltk
 nltk.download()
+
+
+#pip3 install Flask torch torchvision nltk
+
+
+#https://medium.com/@vinitasilaparasetty/how-to-install-numpy-scipy-matplotlib-pandas-scikit-learn-on-windows-46bb6e3e66cf
+#pip3 install numpy
+
+#pip3 install scipy
+
+
+#pip3 install matplotlib
+#pip3 install pandas
+#pip3 install scikit-learn
+
+
+
+#pip3 install -U pip setuptools Wheel
+
+
+
+
+#downloader
+#pip3 install gdown
+#pip3 install JPype1
+
+
+#zemberek
+#https://github.com/ozturkberkay/Zemberek-Python-Examples
+
+
 
