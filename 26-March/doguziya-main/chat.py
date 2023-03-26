4ecc5e6d1ad14042f225f020ee0518c67a3bfa2c
ziyasarican <87414202+ziyasarican@users.noreply.github.com>
2023-03-26 15:09:26 +0300

    Add files via upload

---
+++ 26-March/doguziya-main/chat.py
@@ -0,0 +1,150 @@
+import random
+import json
+import torch
+
+#zemberek cümle düzeltme
+from zemberek import TurkishSentenceNormalizer
+from zemberek import TurkishMorphology
+morphology = TurkishMorphology.create_with_defaults()
+normalizer = TurkishSentenceNormalizer(morphology)
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('Data_JSON/intents.json', encoding='utf-8') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Sam"
+
+"""
+print("Let's chat! (type 'quit' to exit)")
+while True:
+    # sentence = "do you use credit cards?"
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
+             
+"""
+
+
+def get_response(msg): 
+    
+    #translate ile cümle düzeltme
+    #sentence = translate_msg(msg)
+    
+    #zemberek ile cümle düzeltme
+    sentence = normalizer.normalize(msg)
+    print(sentence)
+    
+    
+    sentence = tokenize(sentence)
+    
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    
+    
+    
+    
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                    
+                return random.choice(intent['responses'])
+            
+    return "I dont understand"
+
+
+# düzelt
+def get_id(msg): 
+    sentence = normalizer.normalize(msg)
+    
+    sentence = tokenize(sentence)
+    
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    id_val = intents['intents'][0]['id']
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return intent['id']
+            
+            
+    return "error"
+    
+
+
+
+def translate_msg(msg):
+    from deep_translator import GoogleTranslator
+    translator = GoogleTranslator(source='tr', target='en')
+    temp = translator.translate(msg)
+    translator = GoogleTranslator(source='en', target='tr')
+    temp = translator.translate(temp)
+    return temp.lower()    
+
+
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        
+        
+
+        print(resp)
+
